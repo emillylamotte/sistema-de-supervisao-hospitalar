7e7b96ea9dd535b85cf7cea905a997b203aab6e2
PedroCorcaque <pedrollcorc@gmail.com>
2021-08-23 15:20:52 -0300

    Teste0.01

--- webserver-page-parts/server.ino
+++ webserver-page-parts/server.ino
@@ -9,67 +9,76 @@ IPAddress subnet(255,255,255,0);
 WiFiServer server(80);
 
 //declara os protótipos das funções
-void requisicao(String);
+// void requisicao(String);
 
 //Pagina HTML
 #include "room.h"
 
 void setup() {
-  Serial.begin(115200);
-  delay(10);
-  Serial.println("");
-  Serial.println("");
-  Serial.print("Conectando a ");
-  Serial.print(ssid);
+    Serial.begin(115200);
+    delay(10);
+    Serial.println("");
+    Serial.println("");
+    Serial.print("Conectando a ");
+    Serial.print(ssid);
 
-  WiFi.begin(ssid, password);
-  WiFi.config(ip, gateway, subnet);
+    WiFi.begin(ssid, password);
+    WiFi.config(ip, gateway, subnet);
 
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Conectando a rede sem fio ");
-  Serial.println(ssid);
-  server.begin();
-  Serial.println("Servidor iniciado");
-  Serial.print("IP para se conectar: ");
-  Serial.print("http://");
-  Serial.println(WiFi.localIP());
+    while (WiFi.status() != WL_CONNECTED){ // Enquanto não conectar
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Conectando a rede sem fio ");
+    Serial.println(ssid);
+    server.begin();
+    Serial.println("Servidor iniciado");
+    Serial.print("IP para se conectar: ");
+    Serial.print("http://");
+    Serial.println(WiFi.localIP());
  }
+
+bool buttonPressed = false;
 
 void loop() {
-  WiFiClient client = server.available();
-  if(!client){
-    return;
-  }
-  Serial.println("Novo cliente");
-  while(!client.available()){
-      delay(1);
-   }
-   String request = client.readStringUntil('\r');
-   requisicao(request);
-   Serial.println(request);
-   client.flush();
-   client.println("HTTP/1.1 200 OK");
-   client.println("Content-Type: text/html");
-   client.println("");
-   client.println(room); 
-   client.println("<div class='leito-aux-on'>");
-   client.println("<h1 class='item-head'>Leito 2</h1>");
-   client.println(" <div class='state'>");
-   client.println(" <p class='aux'>Auxílio</p>");
-   client.println("<p class='emergency'>Emergência</p>");
-   client.println("</div>");
-   client.println("</div>");
-   client.println(roomDown);
-   delay(1);
-   Serial.println("Cliente desconectado");
-   Serial.println("");
- }
+    WiFiClient client = server.available();
+    if(!client){
+        return;
+    }
+    // Serial.println("Novo cliente");
+    // while(!client.available()){
+    //     delay(1);
+    // }
+    // String request = client.readStringUntil('\r');
+    // requisicao(request);
+    if (digitalRead(PORTA_BOTAO) == HIGH) {
+        buttonPressed = true;
+    }
+    if (!buttonPressed) {
+        return;
+    } else {
+      // Serial.println(request);
+      client.flush();
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/html");
+      client.println("");
+      client.println(room); 
+      client.println("<div class='leito-aux-on'>");
+      client.println("<h1 class='item-head'>Leito 2</h1>");
+      client.println(" <div class='state'>");
+      client.println(" <p class='aux'>Auxílio</p>");
+      client.println("<p class='emergency'>Emergência</p>");
+      client.println("</div>");
+      client.println("</div>");
+      client.println(roomDown);
+      // Serial.println("Cliente desconectado");
+      // Serial.println("");
+      buttonPressed = false;
+    }
+}
 
- void requisicao(String request){
+//  void requisicao(String request){
     
- }
+//  }
  
