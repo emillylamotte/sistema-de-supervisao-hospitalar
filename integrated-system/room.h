2fd603e5289625ccc9279b0179839a1a2007d25b
Emilly Lamotte <79487290+emillylamotte@users.noreply.github.com>
2021-09-03 20:20:50 -0300

    Update room.h

--- integrated-system/room.h
+++ integrated-system/room.h
@@ -3,6 +3,7 @@ const char room[] = R"=====(<!DOCTYPE html>
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="refresh" content="2">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@500;700&display=swap" rel="stylesheet">
